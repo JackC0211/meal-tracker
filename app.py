763dbe8b65cef1cf81937d8fdcd1fdff23357d69
Jack Cornell <jackcornell0211@icloud.com>
2025-08-02 15:27:07 +0100

    initial commit of flask meal tracker

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template		# gets flask and templates
+
+app = Flask(__name__) # Creates a web page
+
+@app.route("/") # when someone goes to "/"
+def index():
+	return render_template("index.html") # show this
+
+@app.route("/about") #when someone goes to "/about"
+def about():
+	return render_template("about.html")
+
+if __name__ == "__main__":
+	app.run(debug=True)  # starts website
